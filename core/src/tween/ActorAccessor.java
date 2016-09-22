caa583265aa356cd1013c7e28b03623d3ab86b14
yehiahd <yehia_hd@yahoo.com>
2016-09-22 17:01:02 +0200

    implementing tween accessor

---
+++ core/src/tween/ActorAccessor.java
@@ -0,0 +1,38 @@
+package tween;
+
+import actor.AnimalActor;
+import aurelienribon.tweenengine.TweenAccessor;
+
+/**
+ * Created by yehia on 20/09/16.
+ */
+
+public class ActorAccessor implements TweenAccessor<AnimalActor> {
+
+    public static final int MOVE=5;
+
+    @Override
+    public int getValues(AnimalActor target, int tweenType, float[] returnValues) {
+        switch (tweenType){
+            case MOVE:
+                returnValues[0] = target.getX();
+                returnValues[1] = target.getY();
+                return 2;
+            default:
+                assert false;
+                return -1;
+        }
+    }
+
+    @Override
+    public void setValues(AnimalActor target, int tweenType, float[] newValues) {
+        switch (tweenType){
+            case MOVE:
+                target.setX(newValues[0]);
+                target.setY(newValues[1]);
+                break;
+            default:
+                assert false;
+        }
+    }
+}
